f5ef935a1724dc7ba029e9e5c5fcb8e0886f5409
hendraaagil <hendraaagil@gmail.com>
2023-11-22 22:33:17 +0700

    chore: add tremor lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import { Card, Flex, Metric, ProgressBar, Text } from '@tremor/react'
+
+const CardComp = () => (
+  <Card className="mx-auto max-w-xs">
+    <Text>Sales</Text>
+    <Metric>$ 71,465</Metric>
+    <Flex className="mt-4">
+      <Text>32% of annual target</Text>
+      <Text>$ 225,000</Text>
+    </Flex>
+    <ProgressBar value={32} className="mt-2" />
+  </Card>
+)
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <Flex className="min-h-screen flex-col justify-center space-y-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <CardComp />
+    </Flex>
+  )
 }
