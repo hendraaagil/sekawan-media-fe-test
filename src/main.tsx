a64c075d7f89a5efdba08b702210a87fd66e00bd
hendraaagil <hendraaagil@gmail.com>
2023-11-22 22:18:17 +0700

    chore: config tailwind and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from '@/App'
+
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
